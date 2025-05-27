294ded71606a579f4142487f9dd509da3fd5fc46
lgdd <3501902+lgdd@users.noreply.github.com>
2025-05-27 09:09:09 +0200

    fix: allow http traffic (ingress) to custom ip

--- ec2-docker-compose/security_groups.tf
+++ ec2-docker-compose/security_groups.tf
@@ -20,31 +20,7 @@ resource "aws_vpc_security_group_ingress_rule" "storedog_http_ipv4" {
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "storedog_http_ipv6" {
-  security_group_id = aws_security_group.storedog_sg.id
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-  cidr_ipv6         = "::/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "storedog_https_ipv4" {
-  security_group_id = aws_security_group.storedog_sg.id
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "storedog_https_ipv6" {
-  security_group_id = aws_security_group.storedog_sg.id
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-  cidr_ipv6         = "::/0"
+  cidr_ipv4         = var.my_public_ip_cidr
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
