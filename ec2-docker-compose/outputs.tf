c3d779fc136b370c95f14729931d56e0ca9c75de
lgdd <3501902+lgdd@users.noreply.github.com>
2025-06-02 12:56:03 +0200

    chore: add instance name in outputs

--- ec2-docker-compose/outputs.tf
+++ ec2-docker-compose/outputs.tf
@@ -1,3 +1,7 @@
+output "instance" {
+  value = "${var.ec2_instance_name}-${random_string.storedog_id.result}"
+}
+
 output "ssh" {
   value = "ssh -i ~/.ssh/${var.ec2_key_name} ubuntu@${aws_instance.storedog_ec2.public_ip}"
 }
